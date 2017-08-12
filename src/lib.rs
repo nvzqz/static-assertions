9ffba4ad0d2c88e4eb7451ee6668981c9fdbbb2c
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-08-12 14:38:51 -0400

    Create assert_eq_size macro

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,15 @@
 
 #[doc(hidden)]
 pub extern crate core as _core;
+
+/// Asserts at compile-time that the two types have equal sizes.
+#[macro_export]
+macro_rules! assert_eq_size {
+    ($x:ty, $y:ty) => {
+        #[allow(unused_unsafe)]
+        unsafe {
+            use $crate::_core::mem::{forget, transmute, uninitialized};
+            forget::<$y>(transmute(uninitialized::<$x>()));
+        }
+    }
+}
---
+++ tests/eq_size.rs
@@ -0,0 +1,10 @@
+#![no_std]
+
+#[macro_use]
+extern crate static_assertions;
+
+#[test]
+fn test_eq_size() {
+    assert_eq_size!([u8; 2], u16);
+    assert_eq_size!(u8, u16); // Fails to compile
+}
