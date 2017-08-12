44723583e8f1b00caedbb952af23908d875d2bcb
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-08-12 14:41:17 -0400

    Comment out failing macro
    Serves as an example, but shouldn't be emitted because tests will fail.

--- tests/eq_size.rs
+++ tests/eq_size.rs
@@ -6,5 +6,5 @@ extern crate static_assertions;
 #[test]
 fn test_eq_size() {
     assert_eq_size!([u8; 2], u16);
-    assert_eq_size!(u8, u16); // Fails to compile
+    // assert_eq_size!(u8, u16); // Fails to compile
 }
