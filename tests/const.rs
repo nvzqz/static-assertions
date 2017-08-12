9c40c746ac94bb64fb24ade4fdeb1ff3cd0f3dd2
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-08-12 18:09:23 -0400

    [Tests] Add const fn and associated const tests

--- tests/const.rs
+++ tests/const.rs
@@ -1,5 +1,7 @@
 #![no_std]
 
+#![cfg_attr(feature = "const_fn", feature(const_fn))]
+
 #[macro_use]
 extern crate static_assertions;
 
@@ -9,4 +11,30 @@ fn const_assert() {
 
     const_assert!(FIVE * 2 == 10);
     const_assert!(FIVE > 2);
+}
+
+#[cfg(feature = "const_fn")]
+#[test]
+fn const_fn() {
+    const VALUE: usize = 4;
+
+    const fn value() -> usize {
+        VALUE
+    }
+
+    const_assert!(value() == VALUE);
+}
+
+#[cfg(feature = "nightly")]
+#[test]
+fn assoc_const() {
+    const FOUR: usize = 4;
+    trait Assoc { const VAL: usize; }
+    struct Concrete;
+
+    impl Assoc for Concrete {
+        const VAL: usize = FOUR;
+    }
+
+    const_assert!(Concrete::VAL == FOUR);
 }
