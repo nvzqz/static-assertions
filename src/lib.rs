fb29c6a0077c7b920ade330b3bb34e3d41c909fe
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-08-12 16:16:22 -0400

    Make assert_eq_size iterative
    This both simplifies the definition and makes it so that the macro can't
    hit the recursion limit.

--- src/lib.rs
+++ src/lib.rs
@@ -23,17 +23,13 @@ pub extern crate core as _core;
 /// ```
 #[macro_export]
 macro_rules! assert_eq_size {
-    ($x:ty, $y:ty) => {
+    ($x:ty, $($xs:ty),+) => {
         #[allow(unused_unsafe)]
         unsafe {
             use $crate::_core::mem::{forget, transmute, uninitialized};
-            forget::<$y>(transmute(uninitialized::<$x>()));
+            $(forget::<$xs>(transmute(uninitialized::<$x>()));)+
         }
-    };
-    ($x:ty, $y:ty, $($rest:ty),+) => {
-        assert_eq_size!($x, $y);
-        assert_eq_size!($y, $($rest),+);
-    };
+    }
 }
 
 /// Asserts at compile-time that the values have equal sizes.
