cdbfaa895b3c282fa7d50a4df14ea86fe12552a1
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-08-12 15:08:34 -0400

    Add example to assert_eq_size doc

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,23 @@
 pub extern crate core as _core;
 
 /// Asserts at compile-time that the two types have equal sizes.
+///
+/// # Example
+///
+/// ```
+/// #[macro_use]
+/// extern crate static_assertions;
+///
+/// struct Byte(u8);
+///
+/// fn main() {
+///     assert_eq_size!(Byte, u8);
+///     // assert_eq_size!(Byte, u16); // Fails to compile
+///
+///     // Supports unlimited arguments:
+///     assert_eq_size!([Byte; 4], [u16; 2], u32);
+/// }
+/// ```
 #[macro_export]
 macro_rules! assert_eq_size {
     ($x:ty, $y:ty) => {
