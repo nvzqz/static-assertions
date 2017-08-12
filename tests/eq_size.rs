ade97ce600550a8ca414d79497ff611b1ce53628
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-08-12 14:52:01 -0400

    [Tests] Ensure no 'unused_unsafe' warning is given

--- tests/eq_size.rs
+++ tests/eq_size.rs
@@ -9,4 +9,7 @@ fn test_eq_size() {
     assert_eq_size!([u8; 2], u16, (u8, u8));
     assert_eq_size!([u8; 4], u32, (u16, u8, u8), (u16, u16));
     // assert_eq_size!(u8, u16); // Fails to compile
+
+    #[deny(unused_unsafe)]
+    unsafe { assert_eq_size!(u8, u8); }
 }
