7454ee5cb23c5dd2073b555a8623a5a59ca18eac
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-08-12 15:26:38 -0400

    [Tests] Ensure assert_eq_size doesn't drop its val

--- tests/eq_size.rs
+++ tests/eq_size.rs
@@ -3,6 +3,15 @@
 #[macro_use]
 extern crate static_assertions;
 
+/// A type that panics on drop.
+struct PanicDrop<T>(T);
+
+impl<T> Drop for PanicDrop<T> {
+    fn drop(&mut self) {
+        panic!("Dropped!");
+    }
+}
+
 #[test]
 fn test_eq_size() {
     assert_eq_size!([u8; 2], u16);
@@ -12,4 +21,11 @@ fn test_eq_size() {
 
     #[deny(unused_unsafe)]
     unsafe { assert_eq_size!(u8, u8); }
+}
+
+#[test]
+fn test_eq_size_no_drop() {
+    assert_eq_size!(u32, PanicDrop<u32>);
+    assert_eq_size!(PanicDrop<u32>, u32);
+    assert_eq_size!(PanicDrop<u32>, PanicDrop<u32>);
 }
