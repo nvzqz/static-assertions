cdcd2f337a5be43b59638cc7ded1b26e05abc94c
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-08-13 15:48:47 -0400

    [Tests] Add failure case for assert_impl

--- tests/trait_impl.rs
+++ tests/trait_impl.rs
@@ -5,6 +5,9 @@ extern crate static_assertions;
 
 assert_impl!(byte; u8, Send, Sync);
 
+#[cfg(feature = "failure")]
+assert_impl!(ptr; *const u8, Send, Sync);
+
 #[test]
 fn str_impl() {
     assert_impl!(str, Send, Sync, AsRef<[u8]>);
