22876de876265acb5e5596608cd0e7f78522f66c
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-12-11 05:44:09 -0500

    Add examples to assert_fields macro

--- src/lib.rs
+++ src/lib.rs
@@ -368,7 +368,37 @@ macro_rules! assert_obj_safe {
 
 /// Asserts at compile-time that the type has the given fields.
 ///
-/// This is useful for when types have odd fields as a result of `#[cfg]`.
+/// # Examples
+///
+/// This may be used when types have odd fields as a result of `#[cfg]`.
+///
+/// ```
+/// # #[macro_use] extern crate static_assertions;
+/// # fn main() {
+/// struct Ty {
+///     #[cfg(windows)]
+///     value: u8,
+///     #[cfg(not(windows))]
+///     value: usize,
+/// }
+///
+/// /* ... */
+///
+/// // Always have `value` regardless of OS
+/// assert_fields!(Ty, value);
+/// # }
+/// ```
+///
+/// Range does not have a field named `middle`:
+///
+/// ```compile_fail
+/// # #[macro_use] extern crate static_assertions;
+/// # fn main() {
+/// use std::ops::Range;
+///
+/// assert_fields!(Range<u32>, middle);
+/// # }
+/// ```
 #[macro_export]
 macro_rules! assert_fields {
     ($t:path, $($f:ident),+) => {
