898f9cfc90d897cac93feb3997251ad008bc7a17
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-12-11 02:32:41 -0500

    Add assert_cfg macro
    Ensures that the given configuration passes compile-time.

--- src/lib.rs
+++ src/lib.rs
@@ -200,6 +200,46 @@
 #[doc(hidden)]
 pub extern crate core as _core;
 
+/// Asserts that the configuration is set.
+///
+/// # Examples
+///
+/// A project may not support a set of configurations and thus you may want to
+/// report why:
+///
+/// ```
+/// # #[macro_use]
+/// # extern crate static_assertions;
+/// // We should only be compiling for Unix or Linux
+/// # #[cfg(any(unix, linux))]
+/// assert_cfg!(any(unix, linux));
+/// # fn main() {}
+/// ```
+///
+/// If users need to specify a database back-end:
+///
+/// ```
+/// # #[macro_use]
+/// # extern crate static_assertions;
+/// # #[cfg(target_pointer_width = "0")] // Impossible
+/// assert_cfg!("Must exclusively use MySQL or MongoDB as database back-end",
+///             all(not(all(feature = "mysql", feature = "mongodb")),
+///                 any(    feature = "mysql", feature = "mongodb")));
+/// # fn main() {}
+/// ```
+#[macro_export]
+macro_rules! assert_cfg {
+    () => {};
+    ($msg:expr, $($cfg:tt)*) => {
+        #[cfg(not($($cfg)*))]
+        compile_error!($msg);
+    };
+    ($($cfg:tt)*) => {
+        #[cfg(not($($cfg)*))]
+        compile_error!(concat!("Cfg does not pass: ", stringify!($($cfg)*)));
+    };
+}
+
 /// Asserts at compile-time that the types have equal sizes.
 ///
 /// # Example
